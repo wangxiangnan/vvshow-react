b6eda8cdc4f297fad5e5fbd705a4035e78289f47
wangyuda <393640834@qq.com>
2018-03-02 15:33:11 +0800

    modified app.css

--- src/components/footer/views/container.js
+++ src/components/footer/views/container.js
@@ -1,65 +1,65 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, {Component} from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 const ActiveStyle = {
-  color: "#ea5e8b"
+	color: "#ea5e8b"
 };
 
 class Footer extends Component{
 
-  constructor(props){
-    super(props);
-    this.handleScroll = this.handleScroll.bind(this);
-    this.state = {
-      hidden: false,
-      scrollTop: 0
-    }
-  }
+	constructor(props){
+		super(props);
+		this.handleScroll = this.handleScroll.bind(this);
+		this.state = {
+			hidden: false,
+			scrollTop: 0
+		};
+	}
 
-  componentDidMount(){
-    window.addEventListener('scroll', this.handleScroll, false);
-  }
+	componentDidMount(){
+		window.addEventListener("scroll", this.handleScroll, false);
+	}
 
-  componentWillUnmount(){
-    window.removeEventListener('scroll', this.handleScroll);
-  }
+	componentWillUnmount(){
+		window.removeEventListener("scroll", this.handleScroll);
+	}
 
-  handleScroll(e){
-    let curScrollTop = document.documentElement.scrollTop;
-    if(curScrollTop > this.state.scrollTop){ //往上划
-      !this.state.hidden && this.setState({
-        hidden: true
-      })
-    }else{
-      this.state.hidden && this.setState({
-        hidden: false
-      })
-    }
+	handleScroll(e){
+		let curScrollTop = document.documentElement.scrollTop;
+		if(curScrollTop > this.state.scrollTop){ //往上划
+			!this.state.hidden && this.setState({
+				hidden: true
+			});
+		}else{
+			this.state.hidden && this.setState({
+				hidden: false
+			});
+		}
 
-    this.setState({
-      scrollTop: curScrollTop
-    });
-  }
+		this.setState({
+			scrollTop: curScrollTop
+		});
+	}
 
-  render(){
-    return (
-      <div className="footer" style={{display: this.state.hidden? 'none': ''}}>
-        {
-          this.props.tabBars.map(item => (
-            <Link to={item.pagePath} replace key={item.id} className="footer-link">
-              <img src={item.active? item.selectedIconPath: item.iconPath} className="footer-link-icon" />
-              <span style={item.active? ActiveStyle: {}} className="footer-link-text">{item.text}</span>
-            </Link>
-          ))
-        }
+	render(){
+		return (
+			<div className="footer" style={{display: this.state.hidden? "none": ""}}>
+				{
+					this.props.tabBars.map(item => (
+						<Link to={item.pagePath} replace key={item.id} className="footer-link">
+							<img src={item.active? item.selectedIconPath: item.iconPath} className="footer-link-icon" />
+							<span style={item.active? ActiveStyle: {}} className="footer-link-text">{item.text}</span>
+						</Link>
+					))
+				}
 
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => ({
-  tabBars: state.tabBars
+	tabBars: state.tabBars
 });
 export default connect(mapStateToProps)(Footer);
--- src/components/footer/views/item.js
+++ src/components/footer/views/item.js
@@ -1,15 +1,14 @@
-import React, {Component} from 'react';
-import { imgDirUrl } from '../../../config';
+import React, { Component } from "react";
+import { imgDirUrl } from "../../../config";
+class Item extends Component {
 
-class Item extends Comment{
 
-  
-  render(){
-    <Link to="/mine" className="footer-link">
-      <img  src={`${imgDirUrl}/mine.png`} className="footer-link-icon" />
-      <span className="footer-link-text">我的</span>
-    </Link>
-  }
+	render() {
+		<Link to="/mine" className="footer-link">
+			<img src={`${imgDirUrl}/mine.png`} className="footer-link-icon" />
+			<span className="footer-link-text">我的</span>
+		</Link>;
+	}
 }
 
 export default Item;
